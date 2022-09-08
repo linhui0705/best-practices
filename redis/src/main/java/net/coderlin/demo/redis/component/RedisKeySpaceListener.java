2ff4378d8f4463d1225349d66320eb4fb42161d8
linhui <linhui0705@gmail.com>
2022-09-08 15:39:19 +0800

    feat: 修改包路径

---
+++ redis/src/main/java/net/coderlin/demo/redis/component/RedisKeySpaceListener.java
@@ -0,0 +1,73 @@
+package net.coderlin.demo.redis.component;//package net.coderlin.middleware.demo.redis.component;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.data.redis.connection.Message;
+//import org.springframework.data.redis.listener.*;
+//import org.springframework.stereotype.Component;
+//
+///**
+// * Title: RedisKeySpaceListener
+// * Description:
+// *
+// * @author Lin Hui
+// * Created on 2021/9/18 22:56:09
+// */
+//@Component
+//@Slf4j
+//public class RedisKeySpaceListener extends KeyspaceEventMessageListener {
+//
+////    private String keyspaceNotificationsConfigParameter = "Kx";
+////
+////    @Override
+////    public void setKeyspaceNotificationsConfigParameter(String keyspaceNotificationsConfigParameter) {
+////        this.keyspaceNotificationsConfigParameter = keyspaceNotificationsConfigParameter;
+////    }
+//
+//    /**
+//     * Creates new {@link KeyspaceEventMessageListener}.
+//     *
+//     * @param listenerContainer must not be {@literal null}.
+//     */
+//    public RedisKeySpaceListener(RedisMessageListenerContainer listenerContainer) {
+//        super(listenerContainer);
+//        this.setKeyspaceNotificationsConfigParameter("KEA");
+//    }
+//
+////    private static final Topic TOPIC_ALL_KEYEVENTS1 = new PatternTopic("__keyspace@*__:*");
+////    private static final Topic TOPIC_ALL_KEYEVENTS = new PatternTopic("__keyevent@*__:*");
+//    private static final Topic TOPIC_ALL_KEYEVENTS = new PatternTopic("__key*__:*");
+////    private static final Topic TOPIC_ALL_KEYEVENTS = new PatternTopic("__keyspace@* hello");
+////    private static final Topic KEYEVENT_EXPIRED_TOPIC = new PatternTopic("__keyevent@*__:expired hello");
+////    private static final ObjectMapper MAPPER = new ObjectMapper();
+//
+//    @Override
+//    protected void doRegister(RedisMessageListenerContainer listenerContainer) {
+////        listenerContainer.addMessageListener(this, TOPIC_ALL_KEYEVENTS1);
+//        listenerContainer.addMessageListener(this, TOPIC_ALL_KEYEVENTS);
+////        super.doRegister(listenerContainer);
+//    }
+//
+//    @Override
+//    public void onMessage(Message message, byte[] pattern) {
+//        String channel = new String(message.getChannel());
+//        String body = new String(message.getBody());
+//        String patternStr = new String(pattern);
+//        log.info("RedisKeySpaceListener onMessage. message: [{}]. channel: [{}]. body: [{}]. pattern: [{}]",
+//                message.toString(), channel, body, patternStr);
+////        super.onMessage(message, pattern);
+//    }
+//
+//    @Override
+//    protected void doHandleMessage(Message message) {
+//        String channel = new String(message.getChannel());
+//        String body = new String(message.getBody());
+//        log.info("RedisKeySpaceListener doHandleMessage. message: [{}]. channel: [{}]. body: [{}]",
+//                message.toString(), channel, body);
+//    }
+//
+////    @Override
+////    public void setKeyspaceNotificationsConfigParameter(String keyspaceNotificationsConfigParameter) {
+////        super.setKeyspaceNotificationsConfigParameter(keyspaceNotificationsConfigParameter);
+////    }
+//}
---
+++ rocketmq/src/main/java/net/coderlin/demo/rocketmq/config/RocketMQConfig.java
@@ -0,0 +1,79 @@
+package net.coderlin.demo.rocketmq.config;
+
+import net.coderlin.demo.rocketmq.listener.SimpleMQConsumeBListener;
+import net.coderlin.demo.rocketmq.listener.SimpleMQConsumeListener;
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.MQConsumer;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.MQProducer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
+
+/**
+ * Title: RocketMQConfig
+ * Description:
+ *
+ * @author Lin Hui
+ * Created on 2021/8/21 12:01:59
+ */
+@Configuration
+public class RocketMQConfig {
+
+    @Value("${rocketmq.namesrvaddr:localhost:9876}")
+    private String namesrvAddr;
+
+    @Resource
+    private SimpleMQConsumeListener listener;
+
+    @Resource
+    private SimpleMQConsumeBListener bListener;
+
+    @Bean(name = "simpleMQProducer")
+    public MQProducer simpleMQProducer() throws MQClientException {
+        // 实例化消息生产者Producer
+        DefaultMQProducer producer = new DefaultMQProducer("GID_SELF_TEST");
+        // 设置NameServer的地址
+        producer.setNamesrvAddr(namesrvAddr);
+        // 启动Producer实例
+        producer.start();
+        return producer;
+    }
+
+    @Bean(name = "simpleMQConsumer")
+    public MQConsumer simpleMQConsumer() throws MQClientException {
+        // 实例化消费者
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("GID_SELF_TEST");
+        // 设置NameServer的地址
+        consumer.setNamesrvAddr("localhost:9876");
+        // 订阅一个或者多个Topic，以及Tag来过滤需要消费的消息
+        consumer.subscribe("SELF_TEST_TOPIC", "*");
+        consumer.setMessageListener(listener);
+        // 启动消费者实例
+        consumer.start();
+        return consumer;
+    }
+
+    /**
+     * 同一Topic、不同Group下的消费者
+     *
+     * @return
+     * @throws MQClientException
+     */
+    @Bean(name = "simpleMQBConsumer")
+    public MQConsumer simpleMQBConsumer() throws MQClientException {
+        // 实例化消费者
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("GID_SELF_TEST_B");
+        // 设置NameServer的地址
+        consumer.setNamesrvAddr("localhost:9876");
+        // 订阅一个或者多个Topic，以及Tag来过滤需要消费的消息
+        consumer.subscribe("SELF_TEST_TOPIC", "*");
+        consumer.setMessageListener(bListener);
+        // 启动消费者实例
+        consumer.start();
+        return consumer;
+    }
+}
